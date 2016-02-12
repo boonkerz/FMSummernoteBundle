bc7762ca64d5780493d7f8c6c8a8ad7ddbbc0c4e
boonkerz <info@thomas-peterson.de>
2016-02-13 00:18:20 +0100

    Add MediaBundle

---
+++ Resources/public/plugin/summernote-ext-mediabundle.js
@@ -0,0 +1,33 @@
+(function (factory) {
+    /* global define */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals: jQuery
+        factory(window.jQuery);
+    }
+}(function ($){
+    // template, editor
+    var tmpl = $.summernote.renderer.getTemplate();
+    // add plugin
+    $.summernote.addPlugin({
+        name: 'Media', // name of plugin
+        buttons: { // buttons
+            elfinder: function () {
+                return tmpl.iconButton('fa fa-picture-o', {
+                    event: 'mediabundle',
+                    title: 'Mediabundle',
+                    hide: false
+                });
+            }
+        },
+
+        events: { // events
+            mediabundle: function (event, editor, layoutInfo) {
+                mediaBundleBrowser();
+            }
+
+        }
+    });
+}));
